fa4acc7617ac3f185da0ea1f7b697dd2c4cd5f37
ShxwndaPlaya <psverse@outlook.com>
2025-11-10 10:47:44 +0200

    Create HomeIcon.jsx

---
+++ client/src/assets/icons/HomeIcon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function HomeIcon({ size = 24, className = "" }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+    >
+      <path d="M3 12l9-9 9 9" />
+      <path d="M9 21V9h6v12" />
+    </svg>
+  );
+}
