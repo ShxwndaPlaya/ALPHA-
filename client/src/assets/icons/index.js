0cad36b6a0f645b1202417de1fb3c83d9b498a0d
ShxwndaPlaya <psverse@outlook.com>
2025-11-10 10:50:31 +0200

    Create index.js

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,5 @@
+export { default as LogoIcon } from "./LogoIcon.jsx";
+export { default as HomeIcon } from "./HomeIcon.jsx";
+export { default as ChatIcon } from "./ChatIcon.jsx";
+export { default as CodeIcon } from "./CodeIcon.jsx";
+export { default as ShopIcon } from "./ShopIcon.jsx";
